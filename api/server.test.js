c30dfbbee38e887fd2816f35674b3b91d9b224e8
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-20 14:12:08 -0500

    tests wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,39 @@
-// Write your tests here
+const server = require("./server.js")
+const db = require("../data/dbConfig.js")
+const supertest = require("supertest")
+
+beforeEach(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+describe("user tests", () => {
+  it("new user registers", async () => {
+    const res = await supertest(server).post("/api/auth/register").send({ username: 'testUser', password: 'testPassword' })
+
+    expect(res.statusCode).toBe(201)
+    expect(res.body.id).toBe(1)
+    expect(res.body.username).toBe("testUser")
+  })
+
+  it("checks that the user already exists before logging in", async () => {
+    const res = await supertest(server).post("/api/auth/register").send({ username: 'testUser', password: 'testPassword' })
+
+    expect(res.statusCode).toBe(201)
+    expect(res.body.id).toBe(1)
+    expect(res.body.username).toBe('testUser')
+
+    const res3 = await supertest(server).post("/api/auth/login").send({ username: 'userNotInDB', password: 'testPassword' })
+
+    expect(res3.statusCode).toBe(401)
+    expect(res3.body.message).toBe('invalid credentials')
+  })
 })
